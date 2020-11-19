fad27031fc50adb09489e83150ac07029736844a
Exokem <ex010@live.com>
2020-11-19 01:56:25 -0500

    Added QuadTree extract methods to facilitate the extraction of a pixel grid from a QuadTree structure

--- src/model/QuadTree.java
+++ src/model/QuadTree.java
@@ -1,7 +1,5 @@
 package model;
 
-import javafx.scene.Node;
-
 import java.util.List;
 
 public class QuadTree {
@@ -11,6 +9,32 @@ public class QuadTree {
             return new RITQTNode(lineValues.remove(0));
         } else {
             return new RITQTNode(lineValues.remove(0), fromContents(lineValues), fromContents(lineValues), fromContents(lineValues), fromContents(lineValues));
+        }
+    }
+
+    public static int[][] extract(RITQTNode root, int dimension) {
+
+        int[][] pixelGrid = new int[dimension][dimension];
+
+        extract(root, pixelGrid, 0, 0, dimension);
+
+        return pixelGrid;
+    }
+
+    private static void extract(RITQTNode root, int[][] pixelGrid, int row, int col, int dimension) {
+        if (0 <= root.getVal()) {
+            for (int subRow = row; subRow < row + dimension; subRow ++) {
+                for (int subCol = col; subCol < col + dimension; subCol ++ ) {
+                    pixelGrid[subRow][subCol] = root.getVal();
+                }
+            }
+        } else {
+            int subDim = dimension / 2;
+
+            extract(root.getUpperLeft(),  pixelGrid, row,          col,          subDim);
+            extract(root.getUpperRight(), pixelGrid, row,          col + subDim, subDim);
+            extract(root.getLowerLeft(),  pixelGrid, row + subDim, col,          subDim);
+            extract(root.getLowerRight(), pixelGrid, row + subDim, col + subDim, subDim);
         }
     }
 }
