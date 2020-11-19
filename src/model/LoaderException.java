43347e2e90e0cd1b3c0c8080f1e629edf31d5b91
Exokem <ex010@live.com>
2020-11-18 23:24:08 -0500

    Added LoaderExceptions - All exceptions that can occur during uncompressed file loading.

---
+++ src/model/LoaderException.java
@@ -0,0 +1,52 @@
+package model;
+
+public class LoaderException extends Exception {
+
+    protected double value;
+
+    protected LoaderException(double value) {
+        this.value = value;
+    }
+
+    protected LoaderException() {
+
+    }
+
+    public static class IntegralColorException extends LoaderException {
+
+        public IntegralColorException(double value) {
+            super(value);
+        }
+
+        @Override
+        public void printStackTrace() {
+            System.out.println("Integral value " + (int) this.value + " exceeds required bounds: [0, 255]");
+        }
+    }
+
+    public static class FileDimensionException extends LoaderException {
+
+        public FileDimensionException(double value) {
+            super(value);
+        }
+
+        @Override
+        public void printStackTrace() {
+            System.out.println("Nonsquare file dimension: " + this.value);
+        }
+    }
+
+    public static class UnreadablePathException extends LoaderException {
+
+        private final String path;
+
+        public UnreadablePathException(String path) {
+            this.path = path;
+        }
+
+        @Override
+        public void printStackTrace() {
+            System.out.println("File or directory does not exist: " + this.path);
+        }
+    }
+}
