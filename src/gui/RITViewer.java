809744b8780e4b53335d3940c9a11ee32079e7cf
Exokem <ex010@live.com>
2020-11-19 00:01:03 -0500

    Moved file loading exception handling from RITViewer to FileLoader and added check skips for compressed file loading

--- src/gui/RITViewer.java
+++ src/gui/RITViewer.java
@@ -25,7 +25,7 @@ public class RITViewer extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        displayImage(stage, loadValues());
+        displayImage(stage, FileLoader.secureFileContents(FileLoader.UNCM, path));
     }
 
     /**
@@ -54,59 +54,13 @@ public class RITViewer extends Application {
         stage.show();
     }
 
-    /**
-     * Loads the contents of the specified file if the file exists.
-     *
-     * <p>The file read is specified as a single command line argument. Any exceptions that are thrown during
-     * file loading should be caught here.</p>
-     *
-     * @return A list of integers read from each line in the file
-     */
-    private List<Integer> loadValues() {
-        try {
-            // Attempt to load the file from the uncompressed image directory
-
-            List<Integer> lineValues = FileLoader.loadFileContents(FileLoader.UNCM, path);
-            double dimension = Math.sqrt(lineValues.size());
-            if(Math.floor(dimension) != dimension) {
-                throw new LoaderException.FileDimensionException(dimension);
-            } else {
-                return lineValues;
-            }
-        } catch (FileNotFoundException f) {
-            // Handle nonexistent file
-
-            f.printStackTrace();
-            System.out.println("File does not exist: " + path);
-            System.exit(-1);
-        } catch (IOException | LoaderException.IntegralColorException | LoaderException.UnreadablePathException | LoaderException.FileDimensionException e) {
-            // Handle generic IOException, unreadable files, nonsquare files, and color value not in valid range
-            // All LoaderException extensions can be handled in the same way because they override printStackTrace
-
-            e.printStackTrace();
-            System.exit(-1);
-        } catch (NumberFormatException n) {
-            // Handle non-integral color values (NumberFormatException thrown when parsing lines as an integer)
-            // If UNCM is null, this clause will not be reached - a null UNCM case is thrown as an
-            // UnreadablePathException during the loadFileContents call that occurs in the try clause and is handled
-            // before this one
-
-            assert FileLoader.UNCM != null;
-            System.out.println("Exception loading file in directory: " + FileLoader.UNCM.getPath() + path + "\nFile contains a non-integral value");
-            System.exit(-1);
-        }
-
-        // An empty list is returned if an exception is caught
-        return new ArrayList<>();
-    }
-
     /** The file to be loaded as an image. **/
     private static String path;
 
     public static void main(String[] args) {
         if (args.length != 1) {
             // Handle missing or invalid argument(s)
-            System.out.println("Usage: RITViewer <filename>\nSpecified file must be within src/images/uncompressed or some subdirectory of it.");
+            System.out.println("Usage: java RITViewer <uncompressed.txt>\nSpecified file must be within src/images/uncompressed or some subdirectory of it.");
         } else {
             path = args[0];
             Application.launch(args);
--- src/model/FileLoader.java
+++ src/model/FileLoader.java
@@ -10,7 +10,54 @@ public class FileLoader {
     public static final URL COMP = FileLoader.class.getClassLoader().getResource("images/compressed/");
     public static final URL UNCM = FileLoader.class.getClassLoader().getResource("images/uncompressed/");
 
-    public static List<Integer> loadFileContents(URL directoryHeader, String path) throws IOException, LoaderException.IntegralColorException, NumberFormatException, LoaderException.UnreadablePathException {
+    /**
+     * Loads the contents of the specified file if the file exists.
+     *
+     * <p>The file read is specified as a single command line argument. Any exceptions that are thrown during
+     * file loading should be caught here.</p>
+     *
+     * @return A list of integers read from each line in the file
+     */
+    public static List<Integer> secureFileContents(URL directoryHeader, String path) {
+        try {
+            // Attempt to load the file from the uncompressed image directory
+
+            List<Integer> lineValues = FileLoader.loadFileContents(directoryHeader, path);
+            double dimension = Math.sqrt(lineValues.size());
+            if(directoryHeader != COMP && Math.floor(dimension) != dimension) {
+                // Do not check dimension when loading compressed files
+                throw new LoaderException.FileDimensionException(dimension);
+            } else {
+                return lineValues;
+            }
+        } catch (FileNotFoundException f) {
+            // Handle nonexistent file
+
+            f.printStackTrace();
+            System.out.println("File does not exist: " + path);
+            System.exit(-1);
+        } catch (IOException | LoaderException.IntegralColorException | LoaderException.UnreadablePathException | LoaderException.FileDimensionException e) {
+            // Handle generic IOException, unreadable files, nonsquare files, and color value not in valid range
+            // All LoaderException extensions can be handled in the same way because they override printStackTrace
+
+            e.printStackTrace();
+            System.exit(-1);
+        } catch (NumberFormatException n) {
+            // Handle non-integral color values (NumberFormatException thrown when parsing lines as an integer)
+            // If UNCM is null, this clause will not be reached - a null UNCM case is thrown as an
+            // UnreadablePathException during the loadFileContents call that occurs in the try clause and is handled
+            // before this one
+
+            assert directoryHeader != null;
+            System.out.println("Exception loading file in directory: " + directoryHeader.getPath() + path + "\nFile contains a non-integral value");
+            System.exit(-1);
+        }
+
+        // An empty list is returned if an exception is caught
+        return new ArrayList<>();
+    }
+
+    private static List<Integer> loadFileContents(URL directoryHeader, String path) throws IOException, LoaderException.IntegralColorException, NumberFormatException, LoaderException.UnreadablePathException {
 
         if(directoryHeader == null) {
             // Do not attempt to load the file with a null directory header
@@ -21,15 +68,22 @@ public class FileLoader {
 
         List<Integer> lineValues = new ArrayList<>();
 
+
+
         if(file.exists()) {
             BufferedReader reader = new BufferedReader(new FileReader(file));
 
             String line;
 
+            if(directoryHeader == COMP) {
+                line = reader.readLine();
+            }
+
             while ((line = reader.readLine()) != null) {
                 int value = Integer.parseInt(line);
 
-                if(value < 0 || 255 < value) {
+                if(directoryHeader != COMP && (value < 0 || 255 < value)) {
+                    // Do not check colors when loading compressed files
                     throw new LoaderException.IntegralColorException(value);
                 }
 
