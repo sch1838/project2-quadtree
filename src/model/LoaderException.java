95cc49017e2fe99c1d4ef07e1ba59ea7457ff6f6
Exokem <ex010@live.com>
2020-11-19 23:32:27 -0500

    Added documentation to LoaderException and subsidiary exception classes

--- src/model/LoaderException.java
+++ src/model/LoaderException.java
@@ -1,5 +1,10 @@
 package model;
 
+/**
+ * The LoaderException class is a container and superclass to exceptions related to file loading.
+ *
+ * @author Samuel Henderson
+ */
 public class LoaderException extends Exception {
 
     protected double value;
@@ -12,6 +17,9 @@ public class LoaderException extends Exception {
 
     }
 
+    /**
+     * IntegralColorExceptions should be thrown when an integral value is not within the range [0, 255].
+     */
     public static class IntegralColorException extends LoaderException {
 
         public IntegralColorException(double value) {
@@ -24,6 +32,9 @@ public class LoaderException extends Exception {
         }
     }
 
+    /**
+     * FileDimensionExceptions should be thrown when the length of an image file is not a perfect square.
+     */
     public static class FileDimensionException extends LoaderException {
 
         public FileDimensionException(double value) {
@@ -36,6 +47,9 @@ public class LoaderException extends Exception {
         }
     }
 
+    /**
+     * UnreadablePathExceptions should be thrown when file paths cannot be read or do not exist.
+     */
     public static class UnreadablePathException extends LoaderException {
 
         private final String path;
@@ -50,6 +64,9 @@ public class LoaderException extends Exception {
         }
     }
 
+    /**
+     * DirectoryCreationExceptions should be thrown during erroneous directory creations.
+     */
     public static class DirectoryCreationException extends LoaderException {
         private final String path;
 
