53eedf70a51df9b1ceba542ee6bac0dd688d3ee9
Exokem <ex010@live.com>
2020-11-19 23:23:36 -0500

    Added documentation to FileLoader

--- src/model/FileLoader.java
+++ src/model/FileLoader.java
@@ -7,9 +7,17 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The FileLoader class provides methods to read and write file data.
+ *
+ * @author Samuel Henderson
+ */
 public class FileLoader {
 
+    /** The directory header for compressed image files. **/
     public static final URL COMP = FileLoader.class.getClassLoader().getResource("images/compressed/");
+
+    /** The directory header for uncompressed image files. **/
     public static final URL UNCM = FileLoader.class.getClassLoader().getResource("images/uncompressed/");
 
     /**
@@ -59,6 +67,17 @@ public class FileLoader {
         return new ArrayList<>();
     }
 
+    /**
+     * Loads the contents of a file at a provided path into a list of integers.
+     *
+     * <p>A URL representing the directory of a file must be provided in order for this method to function. The path
+     * parameter may itself point to a subdirectory following the directoryHeader, but can otherwise simply be the name
+     * and extension of the file to be read.</p>
+     *
+     * @throws LoaderException.IntegralColorException Thrown when a color value is not in the range [0, 255]
+     * @throws NumberFormatException Thrown when a file contains a non-integral value
+     * @throws LoaderException.UnreadablePathException Thrown when the provided path cannot be read
+     */
     private static List<Integer> loadFileContents(URL directoryHeader, String path) throws IOException, LoaderException.IntegralColorException, NumberFormatException, LoaderException.UnreadablePathException {
 
         if (directoryHeader == null) {
@@ -71,11 +90,14 @@ public class FileLoader {
         List<Integer> lineValues = new ArrayList<>();
 
         if (file.exists()) {
+
+            // Open a reader in the file
             BufferedReader reader = new BufferedReader(new FileReader(file));
 
             String line;
 
             if (directoryHeader == COMP) {
+                // Send the first value in a compressed file to RITUncompress so it can be used as necessary
                 RITUncompress.dimension = Integer.parseInt(reader.readLine());
             }
 
@@ -98,6 +120,12 @@ public class FileLoader {
         return lineValues;
     }
 
+    /**
+     * Writes a two dimensional array of integers to a file specified by the provided directoryHeader and path.
+     *
+     * <p>This method is used to call {@link FileLoader#writeFileContents(int[][], URL, String)} with automatic
+     * attention to any thrown exceptions.</p>
+     */
     public static void secureWriteFileContents(int[][] pixelGrid, URL directoryHeader, String path) {
         try {
             writeFileContents(pixelGrid, directoryHeader, path);
@@ -108,6 +136,15 @@ public class FileLoader {
         }
     }
 
+    /**
+     * Writes a two dimensional array of integers to a file specified by the provided directoryHeader and path.
+     *
+     * <p>Each integer in the pixelGrid will be written to its own line in an existent file. If the file does not exist,
+     * this method will attempt to create it.</p>
+     *
+     * @throws LoaderException.UnreadablePathException Thrown when the provided path cannot be read or does not exist
+     * @throws LoaderException.DirectoryCreationException Thrown when a file cannot be created at the provided path
+     */
     private static void writeFileContents(int[][] pixelGrid, URL directoryHeader, String path) throws LoaderException.UnreadablePathException, IOException, LoaderException.DirectoryCreationException {
 
         if (directoryHeader == null) {
@@ -118,17 +155,24 @@ public class FileLoader {
         File file = new File(directoryHeader.getPath() + path);
 
         if (!file.exists()) {
+
+            // Attempt to create file if it does not exist
             if(!file.mkdirs()) {
+
+                // Except when the file cannot be created
                 throw new LoaderException.DirectoryCreationException(file.getPath());
             }
         }
 
+        // Notify user of output location
         System.out.println("Output file: " + file.getPath());
 
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file)));
 
         for (int[] rows : pixelGrid) {
             for (int value : rows) {
+
+                // Write contents to file
                 writer.write("" + value);
                 writer.newLine();
             }
