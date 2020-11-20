864d66a8f1dc7e1b3cc5f2839194b556c7bd02d2
Exokem <ex010@live.com>
2020-11-19 23:08:40 -0500

    Added documentation to QuadTree

--- src/model/QuadTree.java
+++ src/model/QuadTree.java
@@ -2,8 +2,20 @@ package model;
 
 import java.util.List;
 
+/**
+ * The QuadTree class contains utility methods to be used with QuadTree data structures created using {@link RITQTNode}.
+ *
+ * <p>Included functionality: QuadTree parsing from a formatted list of integers, extracting the contents of a QuadTree
+ * into a grid of pixel color values, and producing a preorder String from a root node.</p>
+ *
+ * @author Samuel Henderson
+ */
 public class QuadTree {
 
+    /**
+     * Provides the root {@link RITQTNode} of a QuadTree data structure parsed from a correctly formatted list of
+     * integral values.
+     */
     public static RITQTNode fromContents(List<Integer> lineValues) {
         if(0 <= lineValues.get(0)) {
             return new RITQTNode(lineValues.remove(0));
@@ -12,23 +24,37 @@ public class QuadTree {
         }
     }
 
+    /**
+     * Converts a QuadTree data structure into a grid of pixel color values.
+     *
+     * @param root The root of the QuadTree structure
+     * @param dimension The side length of the grid to be formed
+     */
     public static int[][] extract(RITQTNode root, int dimension) {
 
+        // Initialize and fill grid
         int[][] pixelGrid = new int[dimension][dimension];
-
         extract(root, pixelGrid, 0, 0, dimension);
 
         return pixelGrid;
     }
 
+    /**
+     * Recursively fills a provided grid of integers with values obtained from a QuadTree data structure. This method
+     * should only be called from {@link QuadTree#extract(RITQTNode, int)} or a similarly functioning method.
+     */
     private static void extract(RITQTNode root, int[][] pixelGrid, int row, int col, int dimension) {
         if (0 <= root.getVal()) {
+
+            // Insert dimension * dimension values into the grid based on the provided row and column
             for (int subRow = row; subRow < row + dimension; subRow ++) {
                 for (int subCol = col; subCol < col + dimension; subCol ++ ) {
                     pixelGrid[subRow][subCol] = root.getVal();
                 }
             }
         } else {
+
+            // Extract values from all four subsidiary nodes using halved dimension
             int subDim = dimension / 2;
 
             extract(root.getUpperLeft(),  pixelGrid, row,          col,          subDim);
@@ -38,6 +64,9 @@ public class QuadTree {
         }
     }
 
+    /**
+     * Traverses a QuadTree data structure in preorder starting from a provided root {@link RITQTNode}.
+     */
     public static String preorder(RITQTNode root) {
         if(0 <= root.getVal()) {
             return root.getVal() + " ";
