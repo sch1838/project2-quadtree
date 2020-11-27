35c369c402275e8d44984de362d74533301fdcdf
Exokem <ex010@live.com>
2020-11-27 13:55:03 -0500

    Made the mess into a neat ui

--- src/gui/Display.java
+++ src/gui/Display.java
@@ -1,12 +1,24 @@
 package gui;
 
 import javafx.application.Platform;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
+import javafx.scene.Node;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.TilePane;
 import javafx.scene.shape.Rectangle;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import model.FileLoader;
+
+import java.io.File;
+import java.util.List;
+import java.util.function.Consumer;
 
 public class Display {
+
+    public static final FileChooser FILE_CHOOSER = new FileChooser();
 
     public static boolean validatePaths(String source, String destination) {
         return source != null && destination != null && !source.isEmpty() && !destination.isEmpty();
@@ -16,21 +28,158 @@ public class Display {
         return path != null && !path.isEmpty();
     }
 
+    public static String postFileSelectionAsPath(Stage stage, String title, boolean save, FileChooser.ExtensionFilter... filters) {
+        FILE_CHOOSER.setTitle(title);
+        FILE_CHOOSER.getExtensionFilters().clear();
+
+        if(filters.length == 0) {
+            FILE_CHOOSER.getExtensionFilters().add(Constants.BOTH_FILTERS);
+        } else {
+            FILE_CHOOSER.getExtensionFilters().addAll(filters);
+        }
+
+        File file = save ? FILE_CHOOSER.showSaveDialog(stage) : FILE_CHOOSER.showOpenDialog(stage);
+
+        if (file != null) {
+            return file.getPath();
+        }
+
+        return "";
+    }
+
+    public static void postFileSelection(Stage stage, String title, boolean save, Consumer<String> pathApplicator, FileChooser.ExtensionFilter... filters) {
+        FILE_CHOOSER.setTitle(title);
+        FILE_CHOOSER.getExtensionFilters().clear();
+
+        if(filters.length == 0) {
+            FILE_CHOOSER.getExtensionFilters().add(Constants.BOTH_FILTERS);
+        } else {
+            FILE_CHOOSER.getExtensionFilters().addAll(filters);
+        }
+
+        File file = save ? FILE_CHOOSER.showSaveDialog(stage) : FILE_CHOOSER.showOpenDialog(stage);
+
+        if (file != null) {
+            pathApplicator.accept(file.getPath());
+        }
+    }
+
+    public static void saveAs(Stage stage) {
+        String destination = postFileSelectionAsPath(stage, "Save As", true);
+        destinationPathField.setText(destination);
+        saveToDestination();
+    }
+
+    public static void saveToDestination() {
+        if(!destinationPathField.getText().isEmpty() && !destinationPathField.getText().equals(Constants.NO_PATH)) {
+            if(activeContents != null) {
+                FileLoader.secureWriteFileContents(activeContents, destinationPathField.getText());
+            }
+        }
+    }
+
+    public static List<String> activeContents;
+
+    public static Mode activeMode = Mode.DISPLAY;
+
+    public static int zoom;
+
     public static final BorderPane container = new BorderPane();
     public static final ListView<Label> output = new ListView<>();
 
+    public static final TextField sourcePathField = new TextField(Constants.NO_PATH), destinationPathField = new TextField(Constants.NO_PATH);
+
     static {
-        container.setCenter(new Rectangle(512, 512));
+        // Add and remove rectangle to expand the window to fit an image
+        container.setCenter(new Rectangle(1024, 384));
+        Platform.runLater(() -> container.setCenter(null));
+
         Label welcome = new Label("Loaded Compresora");
-        Display.output.getItems().add(welcome);
-
+        output.getItems().add(welcome);
         Platform.runLater(() -> output.prefHeightProperty().bind(welcome.heightProperty().multiply(7)));
-
         Display.container.setBottom(output);
-
     }
 
     public static void post(String message) {
         output.getItems().add(new Label(message));
+    }
+
+    public static ListView<Node> createButtonDisplay(Stage stage) {
+        ListView<Node> view = new ListView<>();
+
+        GridPane pathDisplay = new GridPane();
+            pathDisplay.setHgap(10);
+            pathDisplay.setVgap(12);
+            pathDisplay.add(new Label("Source: "), 0, 0);
+            pathDisplay.add(new Label("Destination: "), 0, 1);
+            pathDisplay.add(sourcePathField, 1, 0);
+            pathDisplay.add(destinationPathField, 1, 1);
+                Button sourceSelect = new Button("Select Path");
+                    sourceSelect.setOnAction(actionEvent -> postFileSelection(stage, "Select Source File", false, sourcePathField::setText));
+                MenuButton changeDestination = new MenuButton("Select Path");
+                    MenuItem newFile = new MenuItem("New File");
+                        newFile.setOnAction(actionEvent -> postFileSelection(stage, "Destination As", true, destinationPathField::setText));
+                    MenuItem existingFile = new MenuItem("Existing File");
+                        existingFile.setOnAction(actionEvent -> postFileSelection(stage, "Select Destination File", false, destinationPathField::setText));
+                    changeDestination.getItems().addAll(newFile, existingFile);
+            pathDisplay.add(sourceSelect, 2, 0);
+            pathDisplay.add(changeDestination, 2, 1);
+
+            sourceSelect.setMaxWidth(Double.MAX_VALUE);
+
+            GridPane.setHgrow(sourcePathField, Priority.ALWAYS);
+            GridPane.setHgrow(destinationPathField, Priority.ALWAYS);
+
+            GridPane.setHgrow(sourceSelect, Priority.SOMETIMES);
+
+        TilePane actionBox = new TilePane();
+            MenuButton modeSelect = new MenuButton("Mode");
+                CheckMenuItem displayImage = new CheckMenuItem("Display Image");
+                    displayImage.setSelected(true);
+                CheckMenuItem compressSource = new CheckMenuItem("Compress Source");
+                CheckMenuItem uncompressSource = new CheckMenuItem("Uncompress Source");
+                    displayImage.setOnAction(actionEvent -> {
+                        compressSource.setSelected(false);
+                        uncompressSource.setSelected(false);
+                        activeMode = Mode.DISPLAY;
+                    });
+                    compressSource.setOnAction(actionEvent -> {
+                        displayImage.setSelected(false);
+                        uncompressSource.setSelected(false);
+                        activeMode = Mode.COMPRESS;
+                    });
+                    uncompressSource.setOnAction(actionEvent -> {
+                        compressSource.setSelected(false);
+                        displayImage.setSelected(false);
+                        activeMode = Mode.UNCOMPRESS;
+                    });
+                modeSelect.getItems().addAll(displayImage, compressSource, uncompressSource);
+                modeSelect.setMaxWidth(Double.MAX_VALUE);
+            Button run = new Button("Run");
+                run.setMaxWidth(Double.MAX_VALUE);
+            Button save = new Button("Save");
+                save.setMaxWidth(Double.MAX_VALUE);
+            Button saveAs = new Button("Save As");
+                saveAs.setMaxWidth(Double.MAX_VALUE);
+
+            actionBox.getChildren().addAll(modeSelect, run, save, saveAs);
+            actionBox.setHgap(10);
+
+        view.getItems().addAll(pathDisplay, actionBox);
+
+        return view;
+    }
+
+    public static class Constants {
+        public static final String NO_PATH = "No path selected";
+
+        public static final FileChooser.ExtensionFilter UNCOMPRESSED_FILTER = new FileChooser.ExtensionFilter("Uncompressed Image Format", "*.txt");
+        public static final FileChooser.ExtensionFilter COMPRESSED_FILTER = new FileChooser.ExtensionFilter("Compressed Image Format", "*.rit");
+
+        public static final FileChooser.ExtensionFilter BOTH_FILTERS = new FileChooser.ExtensionFilter("Image Format", "*.rit", "*.txt");
+    }
+
+    enum Mode {
+        COMPRESS, UNCOMPRESS, DISPLAY
     }
 }
--- src/gui/RITGUI.java
+++ src/gui/RITGUI.java
@@ -1,20 +1,8 @@
 package gui;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.layout.BorderPane;
-import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-import model.FileLoader;
-import ptui.RITCompress;
-import ptui.RITUncompress;
-
-import java.io.File;
-import java.util.List;
 
 public class RITGUI extends Application {
 
@@ -27,186 +15,14 @@ public class RITGUI extends Application {
 
         this.stage = stage;
 
-        Display.container.setTop(createMenuBar());
+//        Display.container.setTop(createMenuBar());
+        Display.container.setTop(Display.createButtonDisplay(stage));
 
         // Create and set the scene from the top of the pane hierarchy
         Scene mainScene = new Scene(Display.container);
         stage.setScene(mainScene);
         stage.setTitle("Compresora");
         stage.show();
-    }
-
-    private static final FileChooser.ExtensionFilter UNCOMPRESSED_FILTER = new FileChooser.ExtensionFilter("Uncompressed Image Format", "*.txt");
-    private static final FileChooser.ExtensionFilter COMPRESSED_FILTER = new FileChooser.ExtensionFilter("Compressed Image Format", "*.rit");
-
-    private static final String displayFailure = "Could not display image: Unreadable or nonexistent source file";
-
-    private final BorderPane borders = new BorderPane();
-
-    private final FileChooser fileChooser = new FileChooser();
-
-    private String sourcePath, destinationPath;
-
-    private boolean displayAlerts = true;
-
-    private int zoom = 1;
-
-    private List<String> activeContent;
-
-    private MenuBar createMenuBar() {
-        Menu file = new Menu("File");
-
-            Menu open = new Menu("Open...");
-
-            addTooltipMenuItemToMenu(open, "New", "Open an uncompressed image file other than that at the current source path", actionEvent -> {
-                String selection = this.postFileSelectionAsPath(stage, "Open Image File", false, UNCOMPRESSED_FILTER);
-
-                if (!selection.equals("")) {
-                    this.sourcePath = selection;
-
-                    List<Integer> fileContents = FileLoader.secureLoadFileContents(this.sourcePath);
-
-                    this.borders.setCenter(new Group(RITViewer.fillCanvas(fileContents, this.zoom)));
-                }
-            });
-
-            addTooltipMenuItemToMenu(open, "From Source", "Open the uncompressed image at the current source path", actionEvent -> {
-                if (this.sourcePath != null && !this.sourcePath.contains(".rit")) {
-                    List<Integer> fileContents = FileLoader.secureLoadFileContents(this.sourcePath);
-
-                    this.borders.setCenter(new Group(RITViewer.fillCanvas(fileContents, this.zoom)));
-                } else {
-                    showAlert(new Alert(Alert.AlertType.ERROR, displayFailure));
-                }
-            });
-
-            addTooltipMenuItemToMenu(file, "Save", "Save the currently loaded content to the current destination path", actionEvent -> {
-                if(!Display.validatePath(this.destinationPath)) {
-                    String selection = this.postFileSelectionAsPath(stage, "Select Destination Path", true, COMPRESSED_FILTER, UNCOMPRESSED_FILTER);
-                    if(!selection.isEmpty()) {
-                        this.destinationPath = selection;
-                    }
-                }
-
-                if(this.activeContent != null && Display.validatePaths(this.sourcePath, this.destinationPath)) {
-                    FileLoader.secureWriteFileContents(this.activeContent, this.destinationPath);
-                }
-            });
-            addTooltipMenuItemToMenu(file, "Save As", "Save the currently loaded content to a different path than the current destination path", actionEvent -> {
-                if(this.activeContent != null) {
-                    String selection = this.postFileSelectionAsPath(stage, "Select Destination Path", true, COMPRESSED_FILTER, UNCOMPRESSED_FILTER);
-                    if(!selection.isEmpty()) {
-                        this.destinationPath = selection;
-                        FileLoader.secureWriteFileContents(this.activeContent, this.destinationPath);
-                    }
-                } else {
-                    showAlert(new Alert(Alert.AlertType.ERROR, "Save failed: null content"));
-                }
-            });
-
-        file.getItems().addAll(open, new SeparatorMenuItem(), createSettings());
-
-        Menu edit = new Menu("Edit");
-
-            Menu path = new Menu("Path...");
-
-            addTooltipMenuItemToMenu(path, "Source", "Edit the source path", actionEvent -> {
-                String selection = this.postFileSelectionAsPath(stage, "Select Source File", false, COMPRESSED_FILTER, UNCOMPRESSED_FILTER);
-                if (!selection.isEmpty()) {
-                    this.sourcePath = selection;
-                    showAlert(new Alert(Alert.AlertType.INFORMATION, "The source path has been updated: \n" + selection));
-                }
-            });
-
-            addTooltipMenuItemToMenu(path, "Destination", "Edit the destination path", actionEvent -> {
-                String selection = this.postFileSelectionAsPath(stage, "Select Destination File", false, COMPRESSED_FILTER, UNCOMPRESSED_FILTER);
-                if (!selection.isEmpty()) {
-                    this.destinationPath = selection;
-                    showAlert(new Alert(Alert.AlertType.INFORMATION, "The destination path has been updated: \n" + selection));
-                }
-            });
-
-            Menu compress = new Menu("Compression...");
-
-            addTooltipMenuItemToMenu(compress, "Compress", "Compress the uncompressed image file at the current source path", actionEvent -> {
-                if(Display.validatePath(this.sourcePath)) {
-                    this.activeContent = RITCompress.compress(this.sourcePath);
-                }
-            });
-
-            addTooltipMenuItemToMenu(compress, "Uncompress", "Uncompress the compressed image file at the current source path", actionEvent -> {
-                if(Display.validatePath(this.sourcePath)) {
-                    this.activeContent = RITUncompress.uncompress(this.sourcePath);
-                }
-            });
-
-        edit.getItems().addAll(path, compress);
-
-        Menu view = new Menu("View");
-
-            MenuItem zoom = new MenuItem("Zoom");
-            zoom.setOnAction(actionEvent -> {
-                TextInputDialog prompt = new TextInputDialog("1");
-                prompt.setContentText("Enter a zoom amount: [1, 100]");
-                prompt.showAndWait();
-                String input = prompt.getEditor().getText();
-                try {
-                    int val = Integer.parseInt(input);
-                    this.zoom = Math.max(1, val);
-                    this.zoom = Math.min(100, this.zoom);
-                } catch (NumberFormatException e) {
-                    System.out.println("Failed to update zoom value");
-                }
-            });
-
-        view.getItems().addAll(zoom);
-
-        Menu help = new Menu("Help");
-
-        return new MenuBar(file, edit, view, help);
-    }
-
-    private void showAlert(Alert alert) {
-        if(this.displayAlerts) {
-            alert.show();
-        }
-    }
-
-    private Menu createSettings() {
-        Menu settings = new Menu("Settings");
-        CheckMenuItem alerts = new CheckMenuItem("Show Alerts");
-        alerts.setSelected(true);
-        alerts.setOnAction(actionEvent -> {
-            this.displayAlerts = !this.displayAlerts;
-        });
-
-        settings.getItems().add(alerts);
-        return settings;
-    }
-
-    private String postFileSelectionAsPath(Stage stage, String title, boolean save, FileChooser.ExtensionFilter... filters) {
-        this.fileChooser.setTitle(title);
-        this.fileChooser.getExtensionFilters().clear();
-        this.fileChooser.getExtensionFilters().addAll(filters);
-
-        File file = save ? this.fileChooser.showSaveDialog(stage) : this.fileChooser.showOpenDialog(stage);
-
-        if (file != null) {
-            return file.getPath();
-        }
-
-        return "";
-    }
-
-    private void addTooltipMenuItemToMenu(Menu menu, String itemName, String tooltip, EventHandler<ActionEvent> eventHandler) {
-        CustomMenuItem item = new CustomMenuItem(new Label(itemName));
-        installTooltipMenuItem(menu, item, tooltip, eventHandler);
-    }
-
-    private void installTooltipMenuItem(Menu menu, CustomMenuItem item, String tooltip, EventHandler<ActionEvent> eventHandler) {
-        Tooltip.install(item.getContent(), new Tooltip(tooltip));
-        item.setOnAction(eventHandler);
-        menu.getItems().add(item);
     }
 
     public static void main(String[] args) {
